3a8f202b57bdde536c2be34d00cbf8a2fb7ce6c0
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2023-12-06 08:49:33 +0900

    init commit

---
+++ src/main/java/com/example/gojghobatch/GojghoBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.gojghobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojghoBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GojghoBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gojghobatch/GojghoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gojghobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GojghoBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
